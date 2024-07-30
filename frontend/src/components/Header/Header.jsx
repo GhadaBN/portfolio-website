0c3f6a3845f5fe906f8173f1cab9ee11b85accb7
Ghada Bennasr <gh.bennasr@gmail.com>
2024-07-30 20:52:50 +0200

    setup project page and routes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 import { assets } from "../../assets/assets";
 
@@ -9,71 +10,91 @@ const Header = () => {
         <div id="line-1" className="line">
           <ul className="">
             <li>
-              <span className="title-project">TOMATO SOCIAL</span>
-              <img src={assets.hot_sticker} className="hot_sticker" />
-              <span className="title-project">CLUB</span>
+              <Link to="/project/tomato-social-club">
+                <span className="title-project">TOMATO SOCIAL</span>
+                <img src={assets.hot_sticker} className="hot_sticker" />
+                <span className="title-project">CLUB</span>
+              </Link>
             </li>
           </ul>
         </div>
         <div id="line-2" className="line">
           <ul className="">
             <li>
-              <img src={assets.heart_wings_icon} className="icon" />
-              <span className="title-project">POUR DECISIONS</span>
-              <img src={assets.pour_decisions_icon} className="thumbnail" />
+              <Link to="/project/pour-decisions">
+                <img src={assets.heart_wings_icon} className="icon" />
+                <span className="title-project">POUR DECISIONS</span>
+                <img src={assets.pour_decisions_icon} className="thumbnail" />
+              </Link>
             </li>
           </ul>
         </div>
         <div id="line-3" className="line">
           <ul className="">
             <li>
-              <span className="title-project">FLUFFY DOG</span>
-              <img src={assets.fluffy_dog_icon} className="thumbnail" />
+              <Link to="/project/fluffy-dog">
+                <span className="title-project">FLUFFY DOG</span>
+                <img src={assets.fluffy_dog_icon} className="thumbnail" />
+              </Link>
             </li>
             <li>
-              <span className="title-project">DEV ROAST</span>
-              <img src={assets.thunderbolt_icon} className="icon" />
+              <Link to="/project/dev-roast">
+                <span className="title-project">DEV ROAST</span>
+                <img src={assets.thunderbolt_icon} className="icon" />
+              </Link>
             </li>
           </ul>
         </div>
         <div id="line-4" className="line">
           <ul className="">
             <li>
-              <img src={assets.arrow_icon} className="icon" />
-              <span className="title-project">GORILLA DELIVERY</span>
-              <img src={assets.gorillas_icon} className="thumbnail" />
+              <Link to="/project/gorillas-delivery">
+                <img src={assets.arrow_icon} className="icon" />
+                <span className="title-project">GORILLAS DELIVERY</span>
+                <img src={assets.gorillas_icon} className="thumbnail" />
+              </Link>
             </li>
           </ul>
         </div>
         <div id="line-5" className="line">
           <ul className="">
             <li>
-              <span className="title-project">BUNDESLIGA</span>
-              <img src={assets.football_icon} className="icon" />
+              <Link to="/project/bundesliga">
+                <span className="title-project">BUNDESLIGA</span>
+                <img src={assets.football_icon} className="icon" />
+              </Link>
             </li>
             <li>
-              <span className="title-project">ADIDAS SS20</span>
+              <Link to="/project/adidas">
+                <span className="title-project">ADIDAS SS20</span>
+              </Link>
             </li>
           </ul>
         </div>
         <div id="line-6" className="line">
           <ul className="">
             <li>
-              <span className="title-project">GORILLAS EOY</span>
-              <img src={assets.banana_icon} className="icon" />
+              <Link to="/project/gorillas-eoy">
+                <span className="title-project">GORILLAS EOY</span>
+                <img src={assets.banana_icon} className="icon" />
+              </Link>
             </li>
           </ul>
         </div>
         <div id="line-7" className="line">
           <ul className="">
             <li>
-              <img src={assets.spiral_icon} className="spiral-icon" />
-              <span className="title-project">BMW</span>
-              <img src={assets.bmw_icon} className="thumbnail" />
+              <Link to="/project/bmw">
+                <img src={assets.spiral_icon} className="spiral-icon" />
+                <span className="title-project">BMW</span>
+                <img src={assets.bmw_icon} className="thumbnail" />
+              </Link>
             </li>
             <li>
-              <span className="title-project">TINDER</span>
-              <img src={assets.love_message_icon} className="love-message" />
+              <Link to="/project/tinder">
+                <span className="title-project">TINDER</span>
+                <img src={assets.love_message_icon} className="love-message" />
+              </Link>
             </li>
           </ul>
         </div>

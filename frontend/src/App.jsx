fbd5c4a5070f4a5e893c5aa67ce6e3bd6add0d4a
Ghada Bennasr <gh.bennasr@gmail.com>
2024-09-26 17:37:08 +0200

    category projects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import About from "./pages/About/About";
+import WebDev from "./pages/WebDev/WebDev";
+import DesignDirection from "./pages/DesignDirection/DesignDirection";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import Project from "./pages/ProjectPage/Project";
 
@@ -28,6 +30,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/webdev" element={<WebDev />} />
+        <Route path="/designdirection" element={<DesignDirection />} />
         <Route path="/project/:projectId" element={<Project />} />
       </Routes>
       <Footer />
---
+++ frontend/src/pages/WebDev/WebDev.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
+const WebDev = () => {
+  return (
+    <div className="header">
+      <div className="hero-frame">
+        <div id="line-1" className="line">
+          <ul className="">
+            <li>
+              <Link to="/project/tomato-social-club">
+                <span className="title-project">TOMATO SOCIAL</span>
+                <img src={assets.hot_sticker} className="hot_sticker" />
+                <span className="title-project">CLUB</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div id="line-2" className="line">
+          <ul className="">
+            <li>
+              <Link to="/project/pour-decisions">
+                <img src={assets.heart_wings_icon} className="icon-start" />
+                <span className="title-project">POUR DECISIONS</span>
+                <img src={assets.pour_decisions_icon} className="thumbnail" />
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div id="line-3" className="line">
+          <ul className="">
+            <li>
+              <Link to="/project/fluffy-dog">
+                <span className="title-project">FLUFFY DOG</span>
+                <img src={assets.fluffy_dog_icon} className="thumbnail" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/project/dev-roast">
+                <span className="title-project">DEV ROAST</span>
+                <img src={assets.thunderbolt_icon} className="icon" />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WebDev;

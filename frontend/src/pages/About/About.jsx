ee87827e4c9e4c124392b5a336ebdfded7f857f4
Ghada Bennasr <gh.bennasr@gmail.com>
2024-09-30 16:57:44 +0100

    about page

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>
-      
+    <div className="header">
+      <div className="hero-frame">
+        <div className="text-about">
+          <h1>HELLO, I AM GHADA </h1>
+          <h1>A DESIGNER AND DEVELOPER FROM TUNISIA, BASED IN BERLIN.</h1>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;

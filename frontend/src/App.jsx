c9687cad771749fe5c8b155e3bb063e9cf6581b0
Ghada Bennasr <gh.bennasr@gmail.com>
2024-10-15 13:13:37 +0200

    footer position

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import { Route, Routes, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
@@ -10,19 +9,6 @@ import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import Project from "./pages/ProjectPage/Project";
 
 const App = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    const currentPath = location.pathname;
-    document.body.className = "";
-
-    if (currentPath === "/") {
-      document.body.classList.add("home-page");
-    } else {
-      document.body.classList.add("project-page");
-    }
-  }, [location]);
-
   return (
     <div className="app">
       <Navbar />
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,6 +4,16 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="footer-links-container">
+        <a
+          href="https://github.com/GhadaBN"
+          target="_blank"
+          rel="noreferrer"
+          className="footer-link"
+        >
+          GitHub
+        </a>
+      </div>
       <div className="circle-container">
         <div className="circle-1"></div>
         <div className="circle-2"></div>

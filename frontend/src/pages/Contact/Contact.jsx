57d2c751006eb80656baf6420d34dfd5f5574027
Ghada Bennasr <gh.bennasr@gmail.com>
2024-10-17 02:33:14 +0200

    contact page route

---
+++ frontend/src/pages/Contact/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Contact.css";
+import { assets } from "../../assets/assets";
+
+const Contact = () => {
+  return (
+    <div>
+      <div className="contact-page">
+        <div className="text-contact-wrapper">
+          <span className="text-contact">Get in touch</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;

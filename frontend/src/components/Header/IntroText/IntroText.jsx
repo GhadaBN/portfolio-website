21bcfe2ab752879f280378cabc0e96db25a4634e
Ghada Bennasr <gh.bennasr@gmail.com>
2024-10-22 22:27:56 +0200

    about text update

--- frontend/src/components/Header/IntroText/IntroText.jsx
+++ frontend/src/components/Header/IntroText/IntroText.jsx
@@ -11,25 +11,32 @@ const IntroText = () => {
             <div className="line-about">
               <span className="text-about">HELLO</span>
               <img
-                src={assets.yellow_shape}
+                src={assets.hand_peace}
                 className="icon-intro-text"
                 alt="Icon"
               />
               <span className="text-about"> I AM GHADA</span>
             </div>
             <div className="line-about">
-              <span className="text-about">A DESIGNER AND </span>
+              <span className="text-about">A DESIGNER AND</span>
             </div>
             <div className="line-about">
-              <span className="text-about">
-                DEVELOPER FROM TUNISIA, BASED IN BERLIN, Aiming
-              </span>
+              <span className="text-about">DEVELOPER FR</span>
+              <img
+                src={assets.yellow_shape}
+                className="yellow-icon"
+                alt="Icon"
+              />
+              <span className="text-about">M</span>
             </div>
             <div className="line-about">
-              <span className="text-about">for</span>
-              <img src={assets.mars_icon} className="mars-icon" alt="Icon" />
-              <span className="text-about">.</span>
+              <span className="text-about">TUNISIA BASED IN</span>
             </div>
+          </div>
+          <div className="line-about">
+            <span className="text-about">BERLIN</span>
+            <img src={assets.mars_icon} className="mars-icon" alt="Icon" />
+            <span className="text-about">.</span>
           </div>
         </div>
       </div>

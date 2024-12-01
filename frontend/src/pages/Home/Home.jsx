46be025f8273af2a3bda4968cbb5a49ff6843eac
Ghada Bennasr <gh.bennasr@gmail.com>
2024-12-01 19:38:44 +0100

    update

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./Home.css";
-import Header from "../../components/Header/Header";
 import IntroText from "../../components/Header/IntroText/IntroText";
 const Home = () => {
   return (
     <div className="home">
       <IntroText />
-      {/* <Header /> */}
     </div>
   );
 };
